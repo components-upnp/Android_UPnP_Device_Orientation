2b7226ed333774f131ac87f849281e16636588c5
MKostiuk <Mat01061995>
2017-06-01 17:10:52 +0200

    Initial commit

---
+++ app/src/main/java/com/example/mkostiuk/android_upnp_device_orientation/upnp/OrientationController.java
@@ -0,0 +1,8 @@
+package com.example.mkostiuk.android_upnp_device_orientation.upnp;
+
+/**
+ * Created by mkostiuk on 01/06/2017.
+ */
+
+public class OrientationController {
+}
---
+++ app/src/main/java/com/example/mkostiuk/android_upnp_device_orientation/upnp/SaveUDN.java
@@ -0,0 +1,57 @@
+package com.example.mkostiuk.android_upnp_device_orientation.upnp;
+
+import android.os.Build;
+import android.os.Environment;
+
+import org.fourthline.cling.model.types.UDN;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.util.UUID;
+
+import xdroid.toaster.Toaster;
+
+/**
+ * Created by mkostiuk on 03/05/2017.
+ */
+
+public class SaveUDN {
+
+    public UDN getUdn() throws IOException {
+
+        UDN ret;
+        File fi;
+
+        if (Build.BRAND.toString().equals("htc_europe"))
+            fi = new File("/mnt/emmc/AndroidRemote/udn.txt");
+        else
+            fi = new File(Environment.getExternalStorageDirectory().getPath() + "/AndroidRemote/udn.txt");
+
+        if (!fi.exists()) {
+            fi.createNewFile();
+            fi.setWritable(true);
+        }
+
+        InputStream is = new FileInputStream(fi);
+        BufferedReader br = new BufferedReader(new InputStreamReader(is));
+        String line = br.readLine();
+
+
+        if ((line != null) || (line == "")) {
+            ret =  UDN.valueOf(line);
+        }
+        else {
+            ret = new UDN(UUID.randomUUID());
+            OutputStream o = new FileOutputStream(fi);
+            o.write(ret.toString().getBytes());
+        }
+        Toaster.toast("UDN: " + ret.toString());
+        return ret;
+    }
+}
