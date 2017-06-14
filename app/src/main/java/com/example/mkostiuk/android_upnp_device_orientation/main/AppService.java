612d1cb2a76bdbd99d377c78adeffdd24fcf61cc
MKostiuk <Mat01061995>
2017-06-14 15:57:47 +0200

    Ajout possibilité d'arrêt du service lors d'un clic sur la notification

--- app/src/main/java/com/example/mkostiuk/android_upnp_device_orientation/main/AppService.java
+++ app/src/main/java/com/example/mkostiuk/android_upnp_device_orientation/main/AppService.java
@@ -1,5 +1,7 @@
 package com.example.mkostiuk.android_upnp_device_orientation.main;
 
+import android.app.IntentService;
+import android.app.PendingIntent;
 import android.app.Service;
 import android.content.Context;
 import android.content.Intent;
@@ -11,8 +13,11 @@ import android.os.Environment;
 import android.os.IBinder;
 import android.os.StrictMode;
 import android.support.annotation.Nullable;
+import android.support.v4.app.NotificationManagerCompat;
+import android.support.v7.app.NotificationCompat;
 import android.widget.Toast;
 
+import com.example.mkostiuk.android_upnp_device_orientation.R;
 import com.example.mkostiuk.android_upnp_device_orientation.upnp.OrientationController;
 
 import org.fourthline.cling.android.AndroidUpnpServiceImpl;
@@ -37,6 +42,10 @@ public class AppService extends Service {
     private com.example.mkostiuk.android_upnp_device_orientation.upnp.Service service;
     private SensorManager sensorManager;
     private Sensor sensor;
+
+    public static final int NOTIFICATION_ID = 1;
+
+    public static final String ACTION_1 = "action_1";
 
     @Nullable
     @Override
@@ -94,8 +103,9 @@ public class AppService extends Service {
             }
         }, 20000);
 
+        displayNotification(this);
 
-        return START_STICKY;
+        return START_NOT_STICKY;
     }
 
     @Override
@@ -109,6 +119,55 @@ public class AppService extends Service {
             Thread.sleep(ms);
         } catch (InterruptedException e1) {
             e1.printStackTrace();
+        }
+    }
+
+
+    //fonction permettant d'afficher une notification test à l'utilisateur lors d'un évènement
+    public static void displayNotification(Context context) {
+
+        Intent action1Intent = new Intent(context, NotificationActionService.class)
+                .setAction(ACTION_1);
+
+        PendingIntent action1PendingIntent = PendingIntent.getService(context, 0,
+                action1Intent, PendingIntent.FLAG_ONE_SHOT | PendingIntent.FLAG_UPDATE_CURRENT);
+
+        android.support.v4.app.NotificationCompat.Builder notificationBuilder =
+                new NotificationCompat.Builder(context)
+                        .setSmallIcon(R.mipmap.ic_launcher_round)
+                        .setContentTitle("Accelerometer")
+                        .setContentText("Cliquer ici pour fermer l'application")
+                        .addAction(new NotificationCompat.Action(R.mipmap.ic_launcher_round,
+                                "Fermer", action1PendingIntent))
+                        .setContentIntent(action1PendingIntent)
+                        .setAutoCancel(true)
+                ;
+
+        NotificationManagerCompat notificationManager = NotificationManagerCompat.from(context);
+        notificationManager.notify(NOTIFICATION_ID, notificationBuilder.build());
+    }
+
+
+    //Classe interne permettant de créer un service gérant les actions faites sur la notification
+    //L'utilisateur peut passer au fichier audio suivant ou précédent
+    public static class NotificationActionService extends IntentService {
+        public NotificationActionService() {
+            super(NotificationActionService.class.getSimpleName());
+        }
+
+        @Override
+        protected void onHandleIntent(Intent intent) {
+            String action = intent.getAction();
+            Toaster.toast("Received notification action: " + action);
+            if (ACTION_1.equals(action)) {
+                // TODO: handle action 1.
+                System.out.println("Action 1 notification");
+
+                Intent it = new Intent(Intent.ACTION_CLOSE_SYSTEM_DIALOGS);
+                getApplicationContext().sendBroadcast(it);
+
+                System.exit(0);
+            }
         }
     }
 }
